34a3322598a7231fe3f3e0344dda5e8bc9a9599d
smieszek13 <mateusz.smiechowski.7@gmail.com>
2025-12-18 17:23:06 +0100

    dodaje implementacje backsubst.c

--- lmp9/src/backsubst.c
+++ lmp9/src/backsubst.c
@@ -4,19 +4,32 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    int i, j;
+    int n = mat->r;
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    if (mat->r != mat->c ||
+        mat->r != b->r ||
+        mat->r != x->r ||
+        b->c != 1 ||
+        x->c != 1) {
+        return 2;
+    }
 
-				return 0;
+    for (i = n - 1; i >= 0; i--) {
+        double s = b->data[i][0];
+
+        for (j = i + 1; j < n; j++) {
+            s -= mat->data[i][j] * x->data[j][0];
+        }
+
+        if (mat->data[i][i] == 0) {
+            return 1;
+        }
+
+        x->data[i][0] = s / mat->data[i][i];
+    }
+
+    return 0;
 }
-
-
